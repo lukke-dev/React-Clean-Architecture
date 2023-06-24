d4761116fa2e22234caf132836248baa0c65075d
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-06-24 01:26:19 -0300

    🐳 chore(Configurações do Jest): Testes
    Adicionado as configurações do jest para testes

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts',
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest',
+  },
+}
