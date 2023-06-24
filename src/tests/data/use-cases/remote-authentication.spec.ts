e670386b2beeaeb75bfb5c19c3d80c353cd43acf
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-06-24 16:31:14 -0300

    🧪 test(Remote Authentication):
    Add Tests to Remote Authenticatio ensure httpPostClient with body

--- src/tests/data/use-cases/remote-authentication.spec.ts
+++ src/tests/data/use-cases/remote-authentication.spec.ts
@@ -1,12 +1,15 @@
 import { HttpPostClient, HttpPostClientParams } from '../../../data/interfaces'
 import { RemoteAuthentication } from '../../../data/use-cases'
 import { faker } from '@faker-js/faker'
+import { mockAuthentication } from '../../domain/mocks'
 
 class HttpPostClientSpy implements HttpPostClient {
   url?: string
+  body?: object
 
   async post(params: HttpPostClientParams): Promise<void> {
     this.url = params.url
+    this.body = params.body
     return Promise.resolve()
   }
 }
@@ -29,7 +32,15 @@ describe('Remote Authentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
     const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
-    await sut.auth()
+    await sut.auth(mockAuthentication())
     expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  test('Should call HttpPostClient with correct body', async () => {
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+    const authenticationParams = mockAuthentication()
+    await sut.auth(authenticationParams)
+    expect(httpPostClientSpy.body).toEqual(authenticationParams)
   })
 })
