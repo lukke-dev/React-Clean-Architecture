90dc007e880af91a11e56dadf6447d14b2e2b3c3
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-06-25 01:12:40 -0300

    ✨ feat(AxiosHttpClient):
    Ensure returns correct values

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -1,8 +1,16 @@
-import { HttpPostClientParams } from '@/data/interfaces'
+import {
+  HttpPostClient,
+  HttpPostClientParams,
+  HttpResponse,
+} from '@/data/interfaces'
 import axios from 'axios'
 
-export class AxiosHttpClient {
-  async post(params: HttpPostClientParams<any>): Promise<void> {
-    await axios.post(params.url, params.body)
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post(params: HttpPostClientParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data,
+    }
   }
 }
