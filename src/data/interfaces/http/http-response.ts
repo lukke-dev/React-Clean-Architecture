0f0465337bf41c84d60d9239528ce81fedf0aaa5
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-06-24 21:04:14 -0300

    ✨ feat(Remote Authentication):
    Ensure RemoteAuthentication throws UnexpectedError on defaultCases

--- src/data/interfaces/http/http-response.ts
+++ src/data/interfaces/http/http-response.ts
@@ -1,7 +1,11 @@
 // * Erro no-unused-vars está acontecendo em qualquer enum no Typescript
 /* eslint-disable no-unused-vars */
 export enum HttpStatusCode {
+  ok = 200,
+  notFound = 404,
   noContent = 204,
+  badRequest = 400,
+  serverError = 500,
   unauthorized = 401,
 }
 
--- src/data/use-cases/remote-authentication.ts
+++ src/data/use-cases/remote-authentication.ts
@@ -1,6 +1,6 @@
 import { AuthenticationParams } from '@/domain/use-cases'
 import { HttpPostClient, HttpStatusCode } from '@/data/interfaces'
-import { InvalidCredentialsError } from '@/domain/errors'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 
 export class RemoteAuthentication {
   constructor(
@@ -15,11 +15,14 @@ export class RemoteAuthentication {
     })
 
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: {
+        break
+      }
       case HttpStatusCode.unauthorized: {
         throw new InvalidCredentialsError()
       }
       default: {
-        return Promise.resolve()
+        throw new UnexpectedError()
       }
     }
   }
